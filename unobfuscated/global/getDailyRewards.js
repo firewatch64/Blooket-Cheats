534b1f32fc4f328b7bc449b8205747ba271250f5
Aerell <aerellmck@gmail.com>
2023-07-27 13:13:45 -0500

    Randomly pick discovery set for daily rewards

--- unobfuscated/global/getDailyRewards.js
+++ unobfuscated/global/getDailyRewards.js
@@ -21,7 +21,10 @@
     else {
         const cache = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache),
             axios = cache.find((x) => x.exports?.a?.get).exports.a;
-        axios.post("https://play.blooket.com/api/playersessions/solo", { gameMode: "Factory", questionSetId: "625db660c6842334835cb4c6" }).then(async ({ data: { t } }) => {
+        axios.post("https://play.blooket.com/api/playersessions/solo", {
+            gameMode: "Factory",
+            questionSetId: ["60101da869e8c70013913b59", "625db660c6842334835cb4c6", "60268f8861bd520016eae038", "611e6c804abdf900668699e3", "60ba5ff6077eb600221b7145", "642467af9b704783215c1f1b", "605bd360e35779001bf57c5e", "6234cc7add097ff1c9cff3bd", "600b1491d42a140004d5215a", "5db75fa3f1fa190017b61c0c", "5fac96fe2ca0da00042b018f", "600b14d8d42a140004d52165", "5f88953cdb209e00046522c7", "600b153ad42a140004d52172", "5fe260e72a505b00040e2a11", "5fe3d085a529560004cd3076", "5f5fc017aee59500041a1456", "608b0a5863c4f2001eed43f4", "5fad491512c8620004918ace", "5fc91a9b4ea2e200046bd49a", "5c5d06a7deebc70017245da7", "5ff767051b68750004a6fd21", "5fdcacc85d465a0004b021b9", "5fb7eea20bd44300045ba495"][Math.floor(Math.random() * 24)]
+        }).then(async ({ data: { t } }) => {
             await axios.post("https://play.blooket.com/api/playersessions/landings", { t });
             const { name, blook: { name: blookUsed } } = await cache.find(x => x.exports.a?.me).exports.a.me({}).catch(() => alert('There was an error getting user data.'));
             await axios.put("https://play.blooket.com/api/users/factorystats", {
